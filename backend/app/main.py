820c4ba3ccad030aa01102acc1ace960bc4eb262
SangHui48 <vodlrm123@naver.com>
2023-10-09 21:42:51 +0900

    다시

--- backend/app/main.py
+++
@@ -1,8 +0,0 @@
-import uvicorn
-from fastapi import FastAPI
-
-app = FastAPI()
-
-def start():
-    """Launched with 'poetry run start' at root level"""
-    uvicorn.run("app.main:app", host="localhost", port=8888, reload=True)
